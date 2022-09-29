b3ed7ea34239262e3df9456c49abc2cc8a48a209
Felix Anyanwu <anyanwufelix@gmail.com>
2022-09-29 05:14:15 +0100

    Search replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = my_list[:]
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new[i] = replace
+        else:
+            new[i] = my_list[i]
+    return(new)
